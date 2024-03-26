b18125e6c74a9684f013a03a514595f4ff6d1f6d
francoc055 <castanedafranco994@gmail.com>
2024-03-26 17:01:03 -0300

    url reset pass

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('users/', views.UserApi.as_view(), name='users_user'),
     path('users/<int:id>/', views.UserApi.as_view(), name='user_detail'),
     path('login/', views.Login.as_view(), name='login'), 
-    path('reset-pass/<int:id>/', views.ResetPassword.as_view(), name='reset_pass'), 
+    path('reset-pass/', views.ResetPassword.as_view(), name='reset_pass'), 
     path('verify/', views.VerifyAccount.as_view(), name='verify_account'), 
 ] 
 
